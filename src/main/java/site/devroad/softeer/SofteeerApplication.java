5db310ed8470ff2230f0aae768895c72c82ac915
rohsik2 <rohsik2@gmail.com>
2023-01-29 23:39:56 +0900

    :sparkles: initializing spring boot application

---
+++ src/main/java/site/devroad/softeer/SofteeerApplication.java
@@ -0,0 +1,13 @@
+package site.devroad.softeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SofteeerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SofteeerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/devroad/softeer/SofteeerApplicationTests.java
@@ -0,0 +1,13 @@
+package site.devroad.softeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SofteeerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
