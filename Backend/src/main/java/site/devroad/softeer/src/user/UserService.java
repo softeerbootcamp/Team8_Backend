6d41663417bcd17555eccbc9304acb7caa4d179e
Toa <ld980105@snu.ac.kr>
2023-01-31 18:36:53 +0900

    feat : userService에 signIn 메서드 추가
    Bcrypt을 이용하여 패스워드를 암호화하여 DB에 저장하고 평문으로 signin에 들어온 패스워드와
    비교하여 sign in을 진행함

--- Backend/src/main/java/site/devroad/softeer/src/user/UserService.java
+++ Backend/src/main/java/site/devroad/softeer/src/user/UserService.java
@@ -1,20 +1,43 @@
 package site.devroad.softeer.src.user;
 
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import site.devroad.softeer.exceptions.CustomException;
+import site.devroad.softeer.exceptions.ExceptionType;
+import site.devroad.softeer.src.user.dto.PostSignInReq;
 import site.devroad.softeer.src.user.dto.PostSignUpReq;
 import site.devroad.softeer.src.user.model.Account;
+import site.devroad.softeer.src.user.model.LoginInfo;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     UserRepo userRepo;
+
     public boolean isEmailExist(String email) {
         return userRepo.findByEmail(email).isPresent();
     }
 
     public void join(PostSignUpReq req) {
         Account student = userRepo.addAccountInfo(req.getName(), req.getPhoneNumber(), "Student");
-        userRepo.addLoginInfo(req.getEmail(), req.getPassword(), student.getId());
+        String hashPassword = BCrypt.hashpw(req.getPassword(), BCrypt.gensalt());
+        userRepo.addLoginInfo(req.getEmail(), hashPassword, student.getId());
+    }
+
+    public String signIn(PostSignInReq req) throws CustomException {
+        String email = req.getEmail();
+        Optional<LoginInfo> loginInfo = userRepo.findByEmail(email);
+        if (loginInfo.isEmpty()) {
+            throw new CustomException(ExceptionType.ACCOUNT_NOT_FOUND);
+        }
+        String password = req.getPassword();
+        boolean authentication = BCrypt.checkpw(password, loginInfo.get().getPassword());
+        if (authentication) {
+            return "magicalJWT";
+        }
+        throw new CustomException(ExceptionType.AUTHENTICATION_FAILED);
     }
 }
