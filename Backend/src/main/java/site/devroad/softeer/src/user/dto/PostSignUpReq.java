edbca11c0ea08722e35c475e3b96beea638cffb1
Toa <ld980105@snu.ac.kr>
2023-01-31 16:47:51 +0900

    feat : 회원가입 관련 dto 추가

---
+++ Backend/src/main/java/site/devroad/softeer/src/user/dto/PostSignUpReq.java
@@ -0,0 +1,48 @@
+package site.devroad.softeer.src.user.dto;
+
+public class PostSignUpReq {
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean hasNull() {
+        if (email == null || name == null || phoneNumber == null || password == null)
+            return true;
+        if (email.equals("") || name.equals("") || phoneNumber.equals("") || password.equals(""))
+            return true;
+        return false;
+    }
+}
---
+++ Backend/src/main/java/site/devroad/softeer/src/user/dto/PostSignUpRes.java
@@ -0,0 +1,7 @@
+package site.devroad.softeer.src.user.dto;
+
+public class PostSignUpRes {
+    private boolean isSuccess;
+    private Long userId;
+
+}
