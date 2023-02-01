0b71989d50e71896e61f7502eb6daf3f94644bef
rohsik2 <rohsik2@gmail.com>
2023-02-01 12:08:17 +0900

    feat : cors 관련 해더를 추가하기 위한 CorsUtility Class 추가.

---
+++ Backend/src/main/java/site/devroad/softeer/config/CorsUtility.java
@@ -0,0 +1,16 @@
+package site.devroad.softeer.config;
+
+import org.springframework.http.HttpHeaders;
+
+public class CorsUtility {
+
+    public static HttpHeaders defaultHeader(){
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Access-Control-Allow-Origin", "*");
+        headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        headers.add("Access-Control-Max-Age", "3600");
+        headers.add("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
+        headers.add("Access-Control-Expose-Headers", "xsrf-token");
+        return headers;
+    }
+}
