3caf6a1a5b348ca650ac965f8b88a6b630312369
jun-min1623 <junmin1623@naver.com>
2023-02-01 11:27:28 +0900

    feat(main.js) : bootstrap 적용

--- Frontend/fe_vue/src/main.js
+++ Frontend/fe_vue/src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import "bootstrap/dist/css/bootstrap.min.css" // [bootstrap]
+import "bootstrap" // [bootstrap]
+import axios from 'axios'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+app.mount('#app')
+app.use(axios)
