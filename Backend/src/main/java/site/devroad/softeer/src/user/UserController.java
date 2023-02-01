27769b109873b5bad7d6a1ee36e273feb4eb7f74
Toa <ld980105@snu.ac.kr>
2023-02-01 17:58:33 +0900

    feat : CustomRes 이용해서 UserController의 response 수정

--- Backend/src/main/java/site/devroad/softeer/src/user/UserController.java
+++ Backend/src/main/java/site/devroad/softeer/src/user/UserController.java
@@ -2,7 +2,6 @@ package site.devroad.softeer.src.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +11,7 @@ import site.devroad.softeer.src.user.dto.PostSignInReq;
 import site.devroad.softeer.src.user.dto.PostSignInRes;
 import site.devroad.softeer.src.user.dto.PostSignUpReq;
 import site.devroad.softeer.src.user.dto.PostSignUpRes;
+import site.devroad.softeer.utility.CustomRes;
 
 @RestController
 public class UserController {
@@ -19,22 +19,22 @@ public class UserController {
     UserService userService;
 
     @PostMapping("/api/user/signup")
-    public ResponseEntity<?> postSignUp(@RequestBody PostSignUpReq postSignUpReq) {
+    public CustomRes<?> postSignUp(@RequestBody PostSignUpReq postSignUpReq) {
         if (postSignUpReq.hasNull())
             return new CustomException(ExceptionType.POST_ACCOUNT_FORM_INVALID).getResponseEntity();
         try {
             userService.join(postSignUpReq);
-            return new ResponseEntity<>(new PostSignUpRes(), HttpStatus.CREATED);
+            return new CustomRes<>(new PostSignUpRes(), HttpStatus.CREATED);
         } catch (CustomException e) {
             return e.getResponseEntity();
         }
     }
 
     @PostMapping("/api/user/signin")
-    public ResponseEntity<?> postSignUp(@RequestBody PostSignInReq postSignInReq) {
+    public CustomRes<?> postSignUp(@RequestBody PostSignInReq postSignInReq) {
         try {
             String jwt = userService.signIn(postSignInReq);
-            return new ResponseEntity<>(new PostSignInRes(jwt), HttpStatus.CREATED);
+            return new CustomRes<>(new PostSignInRes(jwt), HttpStatus.CREATED);
         } catch (CustomException e) {
             return e.getResponseEntity();
         }
