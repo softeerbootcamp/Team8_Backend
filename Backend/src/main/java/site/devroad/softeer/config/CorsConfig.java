f92638a59210489e170871f0f45c91cd4edec047
rohsik2 <rohsik2@gmail.com>
2023-02-02 16:03:39 +0900

    feat : cors pre-flight 처리용 해더를 추가하기 위한 CorsConfig 추가.

---
+++ Backend/src/main/java/site/devroad/softeer/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package site.devroad.softeer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .maxAge(3600); // 3600초 동안 preflight 결과를 캐시에 저장
+    }
+}
