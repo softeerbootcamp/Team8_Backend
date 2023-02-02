ae472a0d66f8cc0851b0ea638eb496989c95946a
jun-min1623 <junmin1623@naver.com>
2023-02-02 16:21:32 +0900

    feat : isLogin store 로 관리하는데, header 에서 store 를 참조안하던 버그 수정

--- Frontend/fe_vue/src/components/header.vue
+++ Frontend/fe_vue/src/components/header.vue
@@ -1,13 +1,13 @@
 <template>
     <router-link to="/" class="mainLogo">DevRoad</router-link>
     <div class="menu">
-        <a v-if="!isLogin">
+        <a v-if="!getIsLogin">
             <router-link to="/signup">회원가입</router-link>
         </a>
-        <a v-if="!isLogin">
+        <a v-if="!getIsLogin">
             <router-link to="/login">로그인</router-link>
         </a>
-        <a v-if="isLogin">
+        <a v-if="getIsLogin">
             <router-link to="/">로그아웃</router-link>
         </a>
 
@@ -16,6 +16,11 @@
 <script>
 export default {
     name: 'Header',
+    computed:{
+        getIsLogin(){
+            return this.$store.isLogin
+        }
+    },
     data() {
         return {
         }
