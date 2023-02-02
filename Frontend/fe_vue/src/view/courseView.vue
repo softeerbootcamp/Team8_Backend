24bcb081cd82e64e8b1688c3320a1d05c7cc6da9
jun-min1623 <junmin1623@naver.com>
2023-02-02 12:24:24 +0900

    feat : 버튼별로 따로 card 출력되게 기능 구현. 아직 모든 데이터를 카드형태하는 부분과, 카드 내용을 넣는 부분이 없습니다.

---
+++ Frontend/fe_vue/src/view/courseView.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="d-grid gap-2 col-6 mx-auto">
+      <div class="card" style="width: 18rem;" v-if="isCardOn">
+        <div class="card-body">
+          <h5 class="card-title"></h5>
+          <p class="card-text"></p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </template>
+  <script>
+  export default {
+    props: ['isCardOn'],
+  };
+  </script>
+  <style>
+  
+  </style>
+  
+  
+  
+  
--- Frontend/fe_vue/src/view/roadmap.vue
+++ Frontend/fe_vue/src/view/roadmap.vue
@@ -4,16 +4,22 @@
     {{ isSuccess }}
 
     <h2>roadmapDetail</h2>
-    <div class="d-grid gap-2 col-6 mx-auto" >
-        <div v-for="subject in roadmapDetail" :key="subject"> 
-            <button class="btn btn-primary ms-3" type="button" v-for="course in subject" :key="course"> 
-                {{ course[0] }}
-            </button>
+    <div class="d-grid gap-2 col-6 mx-auto">
+        <div v-for="subject in roadmapDetail" :key="subject">
+            <router-link :to="{ name: 'CourseView', params: { isCardOn: isCardOn} }">
+                <span v-for="course in subject" :key="course">
+                    <button class="btn btn-primary ms-3" @click="getCourseData(subject)">
+                        {{ course[0] }}
+                    </button>
+                    <router-view v-if="curSubjectId==course[1]"></router-view>
+
+                </span>
+            </router-link>
+
         </div>
 
     </div>
-
-    <button @click="getData" v-if="!roadMapShowClicked">
+    <button @click="getSubData" v-if="!roadMapShowClicked">
         temp btn for getting data
     </button>
 
@@ -26,19 +32,75 @@ export default {
 
     data() {
         return {
-            roadMapShowClicked : false,
-            isSuccess: false,
+            roadMapShowClicked: false,
+            isCardOn: false,
+            success: false,
+            subDataSuccess:false,
+            roadmapId: "temp",
             roadmapDetail: [],
-            roadmapId : "temp"
+
+            curSubjectId : "",
+
+            subjectId: "subidtemp",
+            subjectDetail: []
+
         }
 
     },
     methods: {
-
-        getData: function () {
+        setSubIdFromData(subid) {
+            this.curSubjectId = subid;
+        },
+        /** 
+         * roadmapDetail : 
+         * "subjects" : 
+    {
+        //[name, id]
+        "1" : [["기초프로그래밍", 1001]] ,
+        "2" : [["파이썬 기초", 2001], ["자바의 기초",2002]],
+        "3" : [["자료구조와 알고리즘", 3001], ["운영체제",3002]],
+        "4" : [["김영한의 스프링", 4001 ]]
+    }
+         * 
+         * 
+         * subdetail : 
+         * {
+   "success" : "true",
+   "courses" : 
+   [ 
+                {
+            "subjectId":123, 
+            "courseId":123, 
+            "courseName":"name", 
+            "thumbnailUrl":"url", 
+            "courseUrl":"url", 
+            "explain": "hello"
+        } , { ... } 
+    ]
+   
+}
+         */
+        getCourseData(subject) {
+            this.isCardOn = true;
+            console.log("subject : " +subject);
+            var subid = subject.toString().split(',')[1];
+            this.setSubIdFromData(subid);
+            var vm = this;
+            //api/roadmap/{:roadmapId}/{:subjectId}
+            axios.get('http://127.0.0.1:5000/' + 'api/roadmap/' + this.roadmapId +'/' +subid)
+                .then(function (response) {
+                    console.log(response);
+                    vm.subDataSuccess = response.data.success;
+                    vm.subjectDetail = response.data.courses;
+                })
+                .catch(function (error) {
+                    console.log(error)
+                })
+        },
+        getSubData: function () {
             this.roadMapShowClicked = true;
             var vm = this;
-            axios.get('http://127.0.0.1:5000/'+'api/roadmap/'+this.roadmapId)
+            axios.get('http://127.0.0.1:5001/' + 'api/roadmap/' + this.roadmapId)
                 .then(function (response) {
                     console.log(response);
                     vm.isSuccess = response.data.isSuccess;
