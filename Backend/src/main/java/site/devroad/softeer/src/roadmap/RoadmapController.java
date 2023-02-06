41eb78bf14f41c3cddba1738be8c3321070f2ff8
Toa <ld980105@snu.ac.kr>
2023-02-06 14:40:46 +0900

    feat : subject에 해당하는 courses를 가져오는 메서드 추가

--- Backend/src/main/java/site/devroad/softeer/src/roadmap/RoadmapController.java
+++ Backend/src/main/java/site/devroad/softeer/src/roadmap/RoadmapController.java
@@ -1,13 +1,16 @@
 package site.devroad.softeer.src.roadmap;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import site.devroad.softeer.exceptions.CustomException;
-import site.devroad.softeer.src.roadmap.dto.GetRoadmapRes;
+import site.devroad.softeer.src.course.model.Course;
+import site.devroad.softeer.src.course.service.SubjectService;
+import site.devroad.softeer.src.roadmap.dto.GetRoadmapDetailRes;
+import site.devroad.softeer.src.roadmap.dto.GetSubjectDetailRes;
 import site.devroad.softeer.utility.JwtUtility;
 
 import java.util.List;
@@ -17,10 +20,11 @@ import java.util.Map;
 public class RoadmapController {
 
     private final RoadmapService roadmapService;
+    private final SubjectService subjectService;
 
-    @Autowired
-    public RoadmapController(RoadmapService roadmapService) {
+    public RoadmapController(RoadmapService roadmapService, SubjectService subjectService) {
         this.roadmapService = roadmapService;
+        this.subjectService = subjectService;
     }
 
     @GetMapping("/api/roadmap")
@@ -29,7 +33,18 @@ public class RoadmapController {
             JwtUtility.validateToken(jwt);
             Long accountId = JwtUtility.getAccountId(jwt);
             Map<String, List<List<Object>>> subjects = roadmapService.getSubjects(accountId);
-            return new ResponseEntity<>(new GetRoadmapRes(subjects), HttpStatus.OK);
+            return new ResponseEntity<>(new GetRoadmapDetailRes(subjects), HttpStatus.OK);
+        } catch (CustomException e) {
+            return e.getResponseEntity();
+        }
+    }
+
+    @GetMapping("/api/subject/{subjectId}")
+    public ResponseEntity<?> getSubjectDetail(@RequestHeader(value = "jwt") String jwt, @PathVariable("subjectId") String subjectId) {
+        try {
+            JwtUtility.validateToken(jwt);
+            List<Course> courses = subjectService.getCourses(Long.valueOf(subjectId));
+            return new ResponseEntity<>(new GetSubjectDetailRes(courses), HttpStatus.OK);
         } catch (CustomException e) {
             return e.getResponseEntity();
         }
