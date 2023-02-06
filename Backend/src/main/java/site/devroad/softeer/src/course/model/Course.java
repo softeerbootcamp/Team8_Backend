2705a33a4d179796c2a4d4b505f3f340f981a620
Toa <ld980105@snu.ac.kr>
2023-02-06 15:14:36 +0900

    feat : courseName 필드 추가

--- Backend/src/main/java/site/devroad/softeer/src/course/model/Course.java
+++ Backend/src/main/java/site/devroad/softeer/src/course/model/Course.java
@@ -5,16 +5,8 @@ public class Course {
     Long subjectId;
     String tutorName;
     String thumbnailUrl;
+    String courseName;
     String explain;
     Long languageId;
     String type;
-    public Course(Long id, Long subjectId, String tutorName, String thumbnailUrl, String explain, Long languageId, String type) {
-        this.id = id;
-        this.subjectId = subjectId;
-        this.tutorName = tutorName;
-        this.thumbnailUrl = thumbnailUrl;
-        this.explain = explain;
-        this.languageId = languageId;
-        this.type = type;
-    }
 }
