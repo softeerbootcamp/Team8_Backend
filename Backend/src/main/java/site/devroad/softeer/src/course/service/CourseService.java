15616c14f302ac33786591bfcaaca6eaeaae92cc
Toa <ld980105@snu.ac.kr>
2023-02-07 12:44:45 +0900

    feature : CourseService에 다음 챕터를 받아오는 메서드 추가

--- Backend/src/main/java/site/devroad/softeer/src/course/service/CourseService.java
+++ Backend/src/main/java/site/devroad/softeer/src/course/service/CourseService.java
@@ -2,6 +2,7 @@ package site.devroad.softeer.src.course.service;
 
 import org.springframework.stereotype.Service;
 import site.devroad.softeer.exceptions.CustomException;
+import site.devroad.softeer.exceptions.ExceptionType;
 import site.devroad.softeer.src.course.model.Chapter;
 import site.devroad.softeer.src.course.repository.ChapterRepo;
 import site.devroad.softeer.src.course.repository.CourseRepo;
@@ -9,9 +10,11 @@ import site.devroad.softeer.src.roadmap.dto.subdto.ChapterDetail;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CourseService {
+    public static final Long FINISHED = -1L;
     private final ChapterRepo chapterRepo;
     private final CourseRepo courseRepo;
 
@@ -20,12 +23,36 @@ public class CourseService {
         this.courseRepo = courseRepo;
     }
 
-    public List<ChapterDetail> getChapterDetails(Long courseId, Long accountId) throws CustomException {
+    public List<ChapterDetail> getChapterDetails(Long courseId) throws CustomException {
         List<Chapter> chapters = chapterRepo.findChapterByCourseId(courseId);
         ArrayList<ChapterDetail> chapterDetails = new ArrayList<>();
         for (Chapter chapter : chapters) {
             chapterDetails.add(new ChapterDetail(chapter));
         }
         return chapterDetails;
+    }
+
+    public Optional<Chapter> getNextChapter(Long chapterId) throws CustomException {
+        Optional<Chapter> chapterById = chapterRepo.findChapterById(chapterId);
+        if (chapterById.isEmpty()) {
+            throw new CustomException(ExceptionType.CHAPTER_NOT_FOUND);
+        }
+        Chapter chapter = chapterById.get();
+        Long courseId = chapter.getCourseId();
+        Integer sequence = chapter.getSequence();
+        return chapterRepo.findNextChapter(courseId, sequence + 1);
+    }
+
+    public Boolean getCourseFinished(Long chapterId) throws CustomException {
+        Optional<Chapter> nextChapter = getNextChapter(chapterId);
+        return nextChapter.isEmpty();
+    }
+
+    public Long getNextChapterId(Long chapterId) throws CustomException {
+        Optional<Chapter> nextChapter = getNextChapter(chapterId);
+        if (nextChapter.isPresent()) {
+            return nextChapter.get().getId();
+        }
+        return FINISHED;
     }
 }
