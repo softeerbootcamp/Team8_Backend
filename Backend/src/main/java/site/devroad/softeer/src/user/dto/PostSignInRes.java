89b6baff2ec270d1bade80291f8050927573bca9
Toa <ld980105@snu.ac.kr>
2023-02-07 17:33:58 +0900

    feature : admin 계정 생성

--- Backend/src/main/java/site/devroad/softeer/src/user/dto/PostSignInRes.java
+++ Backend/src/main/java/site/devroad/softeer/src/user/dto/PostSignInRes.java
@@ -3,10 +3,16 @@ package site.devroad.softeer.src.user.dto;
 public class PostSignInRes {
     private boolean success;
     private String jwt;
+    private boolean admin;
 
-    public PostSignInRes(String jwt) {
+    public PostSignInRes(String jwt, boolean admin) {
         this.success = true;
         this.jwt = jwt;
+        this.admin = admin;
+    }
+
+    public boolean isAdmin() {
+        return admin;
     }
 
     public boolean isSuccess() {
