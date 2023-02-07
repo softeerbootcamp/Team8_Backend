1a183c76e84355f7c35f6af768c54cea3fd3e695
Toa <ld980105@snu.ac.kr>
2023-02-07 12:44:16 +0900

    feature : ChapterFinish 여부를 put으로 받는 api 구현

--- Backend/src/main/java/site/devroad/softeer/src/roadmap/RoadmapController.java
+++ Backend/src/main/java/site/devroad/softeer/src/roadmap/RoadmapController.java
@@ -2,16 +2,14 @@ package site.devroad.softeer.src.roadmap;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import site.devroad.softeer.exceptions.CustomException;
 import site.devroad.softeer.src.course.service.CourseService;
 import site.devroad.softeer.src.course.service.SubjectService;
 import site.devroad.softeer.src.roadmap.dto.GetCourseDetailRes;
 import site.devroad.softeer.src.roadmap.dto.GetRoadmapDetailRes;
 import site.devroad.softeer.src.roadmap.dto.GetSubjectDetailRes;
+import site.devroad.softeer.src.roadmap.dto.PutChapterFinishRes;
 import site.devroad.softeer.src.roadmap.dto.subdto.ChapterDetail;
 import site.devroad.softeer.src.roadmap.dto.subdto.CourseDetail;
 import site.devroad.softeer.utility.JwtUtility;
@@ -61,9 +59,26 @@ public class RoadmapController {
         try {
             JwtUtility.validateToken(jwt);
             Long accountId = JwtUtility.getAccountId(jwt);
-            List<ChapterDetail> chapterDetails = courseService.getChapterDetails(Long.valueOf(courseId), accountId);
+            List<ChapterDetail> chapterDetails = courseService.getChapterDetails(Long.valueOf(courseId));
             Long curChapterId = roadmapService.getCurChapterId(accountId);
             return new ResponseEntity<>(new GetCourseDetailRes(chapterDetails, curChapterId), HttpStatus.OK);
+        } catch (CustomException e) {
+            return e.getResponseEntity();
+        }
+    }
+
+    @PutMapping("/api/chapter/{chapterId}")
+    public ResponseEntity<?> finishChapter(@RequestHeader(value = "jwt") String jwt, @PathVariable("chapterId") String chapterId) {
+        try {
+            JwtUtility.validateToken(jwt);
+            Long accountId = JwtUtility.getAccountId(jwt);
+            Long chapterIdL = Long.valueOf(chapterId);
+            roadmapService.setCurChapterId(accountId, chapterIdL);
+            //TODO : 다음 챕터의 id
+            Long nextChapterId = courseService.getNextChapterId(chapterIdL);
+            //TODO : 코스의 총 챕터 개수와 sequence값을 비교하여
+            Boolean courseFinished = courseService.getCourseFinished(chapterIdL);
+            return new ResponseEntity<>(new PutChapterFinishRes(courseFinished, nextChapterId), HttpStatus.ACCEPTED);
         } catch (CustomException e) {
             return e.getResponseEntity();
         }
