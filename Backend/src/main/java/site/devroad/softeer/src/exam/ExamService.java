1f24b00b3bfc4f6a2b14de18d5ffd124752f55b2
rohsik2 <rohsik2@gmail.com>
2023-02-08 12:08:09 +0900

    feat : exam 구매 여부 확인및 구매를 위한 servic, repo 작성

--- Backend/src/main/java/site/devroad/softeer/src/exam/ExamService.java
+++ Backend/src/main/java/site/devroad/softeer/src/exam/ExamService.java
@@ -8,9 +8,12 @@ import site.devroad.softeer.exceptions.CustomException;
 import site.devroad.softeer.exceptions.ExceptionType;
 import site.devroad.softeer.src.course.model.Subject;
 import site.devroad.softeer.src.course.repository.SubjectRepo;
+import site.devroad.softeer.src.exam.dto.subdto.ExamDetail;
 import site.devroad.softeer.src.exam.model.SubmissionType;
 import site.devroad.softeer.src.exam.model.Exam;
 import site.devroad.softeer.src.exam.model.ExamSubmission;
+import site.devroad.softeer.src.user.UserRepo;
+import site.devroad.softeer.src.user.model.Account;
 
 import java.util.Optional;
 
@@ -21,11 +24,13 @@ public class ExamService {
     private ExamRepo examRepo;
     private SubjectRepo subjectRepo;
     private ExamSubmissionRepo examSubmissionRepo;
+    private UserRepo userRepo;
     @Autowired
-    public ExamService(ExamRepo examRepo, SubjectRepo subjectRepo, ExamSubmissionRepo examSubmissionRepo){
+    public ExamService(ExamRepo examRepo, SubjectRepo subjectRepo, ExamSubmissionRepo examSubmissionRepo, UserRepo userRepo){
         this.examRepo = examRepo;
         this.subjectRepo = subjectRepo;
         this.examSubmissionRepo = examSubmissionRepo;
+        this.userRepo = userRepo;
     }
 
     public Boolean isUserPassedTest(Long subjectId, Long accountId) throws CustomException {
@@ -40,9 +45,9 @@ public class ExamService {
         logger.debug("subject id : {}, subject explain : {}",  subject.getId(), subject.getExplain());
 
         //subject.id -> test
-        Optional<Exam> optionalTest = examRepo.findTestBySubjectId(subject.getId());
+        Optional<Exam> optionalTest = examRepo.findExamBySubjectId(subject.getId());
         if(optionalTest.isEmpty())
-            throw new CustomException(ExceptionType.TEST_NOT_FOUND);
+            throw new CustomException(ExceptionType.EXAM_NOT_FOUND);
 
         Exam exam = optionalTest.get();
         logger.debug("test id : {}, test explain : {}, accountId : {}",  exam.getId(), exam.getExplain(), accountId);
@@ -55,4 +60,23 @@ public class ExamService {
 
     }
 
+    public void purchaseExam(Long accountId, Long examId) throws CustomException {
+        Account account = userRepo.findAccountById(accountId);
+        Optional<Exam> exam = examRepo.findExamById(examId);
+        examRepo.addExamToAccount(accountId, examId);
+    }
+
+    public void checkExamPurchased(Long accountId, Long examId) throws CustomException {
+        if(! examRepo.isExamPurchased(accountId, examId)){
+            throw new CustomException(ExceptionType.EXAM_NOT_PURCHASED);
+        }
+    }
+
+    public ExamDetail getExamDetail(Long examId) throws CustomException {
+        Optional<Exam> exam = examRepo.findExamById(examId);
+        if(exam.isEmpty()){
+            throw new CustomException(ExceptionType.EXAM_NOT_FOUND);
+        }
+        return new ExamDetail(exam.get());
+    }
 }
