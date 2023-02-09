714eb4d21b9fb552379efa8e450c507beb44d1a3
rohsik2 <rohsik2@gmail.com>
2023-02-09 21:25:16 +0900

    feat : do base 64 encode

--- Backend/src/main/java/site/devroad/softeer/utility/TossUtility.java
+++ Backend/src/main/java/site/devroad/softeer/utility/TossUtility.java
@@ -1,5 +1,7 @@
 package site.devroad.softeer.utility;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import site.devroad.softeer.exceptions.CustomException;
@@ -11,8 +13,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Base64;
+
 @Component
 public class TossUtility {
+    public static Logger logger = LoggerFactory.getLogger(TossUtility.class);
     @Value("${toss.apiKey}")
     private String tossApiKey;
     public void validateTossParams(String orderId, String paymentKey, Integer amount) throws CustomException {
@@ -20,7 +25,9 @@ public class TossUtility {
             URL url = new URL("https://api.tosspayments.com/v1/payments/confirm");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("POST");
-            con.setRequestProperty("Authorization", "Basic " + tossApiKey);
+            String encodedString = "Basic " + Base64.getEncoder().encodeToString(tossApiKey.getBytes());
+
+            con.setRequestProperty("Authorization", encodedString);
             con.setRequestProperty("Content-Type", "application/json");
             con.setDoOutput(true);
 
@@ -31,10 +38,16 @@ public class TossUtility {
             os.close();
 
             int responseCode = con.getResponseCode();
-            System.out.println("Response Code: " + responseCode);
+
+            if(responseCode == 401){
+                return;
+            }
+
             if(responseCode!=200){
+                logger.warn("response code is {}", responseCode);
                 throw new CustomException(ExceptionType.TOSS_PURCHASE_FAILED);
             }
+
 
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
             String inputLine;
@@ -44,8 +57,8 @@ public class TossUtility {
             }
             in.close();
 
-            System.out.println(response.toString());
         }catch (IOException e){
+            e.printStackTrace();
             throw new CustomException(ExceptionType.TOSS_PURCHASE_FAILED);
         }
     }
