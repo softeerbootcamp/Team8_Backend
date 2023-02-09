97db7cfb87af2b9901015d66835bd94479eb271a
Toa <ld980105@snu.ac.kr>
2023-02-09 11:28:30 +0900

    feat : getExamDetail 메서드 수정

--- Backend/src/main/java/site/devroad/softeer/src/exam/ExamService.java
+++ Backend/src/main/java/site/devroad/softeer/src/exam/ExamService.java
@@ -9,9 +9,9 @@ import site.devroad.softeer.exceptions.ExceptionType;
 import site.devroad.softeer.src.course.model.Subject;
 import site.devroad.softeer.src.course.repository.SubjectRepo;
 import site.devroad.softeer.src.exam.dto.subdto.ExamDetail;
-import site.devroad.softeer.src.exam.model.SubmissionType;
 import site.devroad.softeer.src.exam.model.Exam;
 import site.devroad.softeer.src.exam.model.ExamSubmission;
+import site.devroad.softeer.src.exam.model.SubmissionType;
 import site.devroad.softeer.src.user.UserRepo;
 import site.devroad.softeer.src.user.model.Account;
 
@@ -25,8 +25,9 @@ public class ExamService {
     private SubjectRepo subjectRepo;
     private ExamSubmissionRepo examSubmissionRepo;
     private UserRepo userRepo;
+
     @Autowired
-    public ExamService(ExamRepo examRepo, SubjectRepo subjectRepo, ExamSubmissionRepo examSubmissionRepo, UserRepo userRepo){
+    public ExamService(ExamRepo examRepo, SubjectRepo subjectRepo, ExamSubmissionRepo examSubmissionRepo, UserRepo userRepo) {
         this.examRepo = examRepo;
         this.subjectRepo = subjectRepo;
         this.examSubmissionRepo = examSubmissionRepo;
@@ -37,20 +38,20 @@ public class ExamService {
 
         //subjectId -> subject
         Optional<Subject> optionalSubject = subjectRepo.findById(subjectId);
-        if(optionalSubject.isEmpty())
+        if (optionalSubject.isEmpty())
             throw new CustomException(ExceptionType.SUBJECT_NOT_FOUND);
 
         Subject subject = optionalSubject.get();
 
-        logger.debug("subject id : {}, subject description : {}",  subject.getId(), subject.getDescription());
+        logger.debug("subject id : {}, subject description : {}", subject.getId(), subject.getDescription());
 
         //subject.id -> exam
         Optional<Exam> optionalExam = examRepo.findExamBySubjectId(subject.getId());
-        if(optionalExam.isEmpty())
+        if (optionalExam.isEmpty())
             throw new CustomException(ExceptionType.EXAM_NOT_FOUND);
 
         Exam exam = optionalExam.get();
-        logger.debug("exam id : {}, exam description : {}, accountId : {}",  exam.getId(), exam.getDescription(), accountId);
+        logger.debug("exam id : {}, exam description : {}, accountId : {}", exam.getId(), exam.getDescription(), accountId);
         //exam.id + account.id -> examSubmission
         Optional<ExamSubmission> examSubmission = examSubmissionRepo.findByExamIdAndAccountId(exam.getId(), accountId);
 
@@ -67,16 +68,16 @@ public class ExamService {
     }
 
     public void checkExamPurchased(Long accountId, Long examId) throws CustomException {
-        if(! examRepo.isExamPurchased(accountId, examId)){
+        if (!examRepo.isExamPurchased(accountId, examId)) {
             throw new CustomException(ExceptionType.EXAM_NOT_PURCHASED);
         }
     }
 
     public ExamDetail getExamDetail(Long examId) throws CustomException {
-        Optional<Exam> exam = examRepo.findExamById(examId);
-        if(exam.isEmpty()){
+        Optional<ExamDetail> examDetailById = examRepo.findExamDetailById(examId);
+        if (examDetailById.isEmpty()) {
             throw new CustomException(ExceptionType.EXAM_NOT_FOUND);
         }
-        return new ExamDetail(exam.get());
+        return examDetailById.get();
     }
 }
