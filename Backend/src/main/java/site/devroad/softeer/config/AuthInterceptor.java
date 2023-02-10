4b895b164eb6c3d3836421c488ad2e6161b7b482
Toa <ld980105@snu.ac.kr>
2023-02-10 10:55:56 +0900

    feat : JwtExceptionHandler 구현

---
+++ Backend/src/main/java/site/devroad/softeer/config/AuthInterceptor.java
@@ -0,0 +1,30 @@
+package site.devroad.softeer.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import site.devroad.softeer.utility.JwtUtility;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class AuthInterceptor implements HandlerInterceptor {
+    private static Logger logger = LoggerFactory.getLogger(AuthInterceptor.class);
+    private JwtUtility jwtUtility;
+
+    public AuthInterceptor(JwtUtility jwtUtility) {
+        this.jwtUtility = jwtUtility;
+    }
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        logger.info(">>> interceptor.preHandle 호출");
+        String jwt = request.getHeader("jwt");
+        jwtUtility.validateToken(jwt);
+        Long accountId = jwtUtility.getAccountId(jwt);
+        request.setAttribute("accountId", accountId);
+        return true;
+    }
+}
