cd7c8607f34a5ee37bf83ada0e992a48738f3206
rohsik2 <rohsik2@gmail.com>
2023-02-12 23:00:38 +0900

    feat : print error message when login failed or server error

--- Frontend/fe_vue/src/components/login.vue
+++ Frontend/fe_vue/src/components/login.vue
@@ -1,7 +1,6 @@
 <template>
-  <div class="container center">
     <div class="row">
-      <div class="offset-3 col-6 mt-4">
+      <div class="offset-3 col-6">
         <form @submit.prevent="loginForm" class="col-12">
           <div class="col-12 mb-4">
             <label for="InputEmail" class="form-label">Email address</label>
@@ -12,13 +11,16 @@
             <label for="InputPassword1" class="form-label">Password</label>
             <input type="password" class="form-control" v-model="formData.password" id="InputPassword1" />
           </div>
+          <div class="col-12">
+            <p v-if="isLoginFailed" class="text-center text-danger alert" >{{ failMessage }}</p>
+          </div>
           <div class="offset-3 col-6">
             <button type="submit" class="btn btn-dark d-grid gap-2 mx-auto" variant="primary">Submit</button>
           </div>
+
         </form>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -33,6 +35,8 @@ export default {
         email: "",
         password: "",
       },
+      isLoginFailed: false,
+      failMessage: "",
     };
   },
 
@@ -43,6 +47,18 @@ export default {
         password: this.formData.password
       };
       await signinUser(userData)
+      .catch((error) => {
+        if(error.response.status != 500){
+          this.isLoginFailed = true;
+          this.failMessage = "아이디 또는 비밀번호가 일치하지 않습니다."
+        }
+        else{
+          this.isLoginFailed = true;
+          this.failMessage = "서버에 문제가 발생했습니다. 잠시 후 다시 시도해주세요."
+        }
+
+        console.log("login failed : " + error);
+      })
         .then((response) => {
           if (response.data.success) {
             this.$store.commit("setLoginStatus", true);
@@ -57,6 +73,7 @@ export default {
 
             }
           } else {
+            this.isLoginFailed = true;
             console.log("login failed : " + response.data.success);
           }
         });
@@ -65,6 +82,9 @@ export default {
   },
 };
 </script>
-<style>
 
+<style>
+.alert{
+  font-size: 0.7rem;
+}
 </style>
