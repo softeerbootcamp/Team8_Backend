9191987b6f0270520eec6f6e9b82b9350ae87178
jun-min1623 <junmin1623@naver.com>
2023-02-13 16:32:53 +0900

    feat : chapter frame 동적으로 받아오기 마무리

--- Frontend/fe_vue/src/api/index.js
+++ Frontend/fe_vue/src/api/index.js
@@ -46,9 +46,11 @@ function postAssignMentData(config, param){
 }
 function sendExamResult(config,param){
     return axiosService.put('api/exam/result/',param,config);
-
+}
+function getChapterDetailData(config,param){
+  return axiosService.get('api/chapter/',param,config);
 }
 export { adminUser,signinUser,signupUser,userData,getRoadmap,getSubjectDetail,getCourseData
 ,getAllSubjectData,postRoadmapToUserByEmail,getNoRoadmapUserData,getExamDetailData,
-postAssignMentData,sendExamResult
+postAssignMentData,sendExamResult,getChapterDetailData
  };
--- Frontend/fe_vue/src/view/user/chapterFrame.vue
+++ Frontend/fe_vue/src/view/user/chapterFrame.vue
@@ -1,20 +1,50 @@
 <template>
-    <iframe :src="src" title="YouTube video player" frameborder="0"
+    <iframe :src="chapterUrl" title="YouTube video player" frameborder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" width="560"
         height="315" allowfullscreen />
 
 </template>
 <script>
+import { getChapterDetailData } from '@/api';
 export default {
     name: 'ChapterFrame',
     data: () => ({
         myIframe: null,
-        src: "https://www.youtube.com/embed/dh4hdtZ00EU"
+        chapterId: "",
+        chapterName: "",
+        chapterUrl: "",
+        thumbnailUrl: "",
+        description: "",
+        finish: true
     }),
     methods: {
         onLoad(frame) {
             this.myIframe = frame.contentWindow
-        }
+        },
+        async getChapterDetail() {
+            this.chapterId = this.$route.params.chapterId;
+
+            const config = {
+                headers: {
+                    jwt: this.$store.state.jwt
+                }
+            };
+            await getChapterDetailData(config, this.chapterId)
+                .then((response) => {
+                    if (response.data.success) {
+                        this.chapterName = response.data.chapterName;
+                        this.chapterId = response.data.chapterId;
+                        this.chapterUrl = response.data.chapterUrl;
+                        this.thumbnailUrl = response.data.thumbnailUrl;
+                        this.description = response.data.description;
+                        this.finish = response.data.finish;
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+        },
+
     }
 }
 </script>
