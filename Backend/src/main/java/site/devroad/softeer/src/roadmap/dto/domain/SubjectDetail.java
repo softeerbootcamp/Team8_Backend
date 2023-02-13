749f1f006a172f35f1010191a1df2d7bbfba80f1
Toa <ld980105@snu.ac.kr>
2023-02-13 20:38:58 +0900

    refactor : change mcqState, frqState data type

--- Backend/src/main/java/site/devroad/softeer/src/roadmap/dto/domain/SubjectDetail.java
+++ Backend/src/main/java/site/devroad/softeer/src/roadmap/dto/domain/SubjectDetail.java
@@ -1,14 +1,16 @@
 package site.devroad.softeer.src.roadmap.dto.domain;
 
+import site.devroad.softeer.src.exam.model.SubmissionType;
+
 public class SubjectDetail {
     private String name;
     private Long subjectId;
-    private String mcqState;
-    private String frqState;
-    private String mcqExamId;
-    private String frqExamId;
+    private SubmissionType mcqState;
+    private SubmissionType frqState;
+    private Long mcqExamId;
+    private Long frqExamId;
 
-    public SubjectDetail(String name, Long subjectId, String mcqState, String frqState, String mcqExamId, String frqExamId) {
+    public SubjectDetail(String name, Long subjectId, SubmissionType mcqState, SubmissionType frqState, Long mcqExamId, Long frqExamId) {
         this.name = name;
         this.subjectId = subjectId;
         this.mcqState = mcqState;
@@ -25,19 +27,19 @@ public class SubjectDetail {
         return subjectId;
     }
 
-    public String getMcqState() {
+    public SubmissionType getMcqState() {
         return mcqState;
     }
 
-    public String getFrqState() {
+    public SubmissionType getFrqState() {
         return frqState;
     }
 
-    public String getMcqExamId() {
+    public Long getMcqExamId() {
         return mcqExamId;
     }
 
-    public String getFrqExamId() {
+    public Long getFrqExamId() {
         return frqExamId;
     }
 }
