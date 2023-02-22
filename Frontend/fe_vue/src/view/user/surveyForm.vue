0fa3d08ff5e0d7593a5b934d554657d77058237b
rohsik2 <rohsik2@gmail.com>
2023-02-22 11:56:59 +0900

    feat : adding survery page

--- Frontend/fe_vue/src/view/user/surveyForm.vue
+++ Frontend/fe_vue/src/view/user/surveyForm.vue
@@ -1,6 +1,10 @@
 <template>
-    <div class="row" justify-content-center>
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfrxSZ47GKSVhye3hIXoFVHGmQSvA8yzUEhY01-N91e4Gwtwg/viewform?embedded=true" width="640" height="615" frameborder="0" marginheight="0" marginwidth="0">로드 중…</iframe>
+    <div class="row justify-content-center text-center" justify-content-center>
+        <iframe class="col-12" src="https://docs.google.com/forms/d/e/1FAIpQLSfrxSZ47GKSVhye3hIXoFVHGmQSvA8yzUEhY01-N91e4Gwtwg/viewform?embedded=true" width="640" height="750" frameborder="0" marginheight="0" marginwidth="0">로드 중…</iframe>
+
+            <p>설문조사를 완료하시면 눌러주세요!</p>
+            <button class="col-2 btn btn-dark" @click="this.$router.push({ name: 'UserHome' })">완료</button>      
+
     </div>
 </template>
 
@@ -24,5 +28,7 @@ export default {
 </script>
 
 <style>
-    
+.prime{
+    color : #B01E68;
+}
 </style>
